e14285b6a1ab1b974219370191a4d56058c1bca7
Oshioke-Salaki <salaki1902@gmail.com>
2025-08-15 00:32:33 +0100

    feat: web3 integration

---
+++ src/components/helloTest.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+function helloTest() {
+  const [value, setValue] = useState<string | undefined>(undefined);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    async function fetchValue() {
+      try {
+        setLoading(true);
+        setValue("1");
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchValue();
+  });
+  if (loading) return <div>Loading...</div>;
+  return (
+    <div className="border border-[#c6c6c6] rounded-sm py-2 px-5 font-normal">
+      {value}
+    </div>
+  );
+}
+
+export default helloTest;
