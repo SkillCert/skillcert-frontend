be39232afd528fd761b0d92de9d97aaab223738f
Shyaar Sati Fwatshak <shyaarsati@gmail.com>
2025-09-11 20:49:57 +0100

    feat(error): add custom 500 error page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import Image from "next/image";
 import WelcomePage from "./home/page";
-import ErrorPage from "./error";
-
 
 export default function Page() {
   return (
     <div className="">
-      {/* <WelcomePage /> */}
-      <ErrorPage />
+      <WelcomePage />
     </div>
   );
 }
