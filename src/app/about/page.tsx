70b8857f4fe5000058dec352c26fcc08289cfd55
omobolaji <omobolaji119@gmail.com>
2025-09-19 20:31:48 +0100

    =create the information section of about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,10 @@
 import HeroSection from "./components/heroSection";
+import InformationSection from "./components/informationSection";
 export default function AboutPage() {
   return (
     <>
       <HeroSection />
+      <InformationSection />
     </>
   );
 }
