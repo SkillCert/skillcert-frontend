c27a8f6613775fc142df72fa496f0eba49f30fb2
SaadTech360 <saadibrahimumar7@gmail.com>
2025-10-02 10:12:38 +0100

    make about-us page fully responsive

--- src/app/home/components/aboutUsPage.tsx
+++ src/app/home/components/aboutUsPage.tsx
@@ -27,7 +27,7 @@ const features = [
 
 export default function AboutSkillCert() {
   return (
-    <div className="bg-gray-900 text-white px-[6.8em] h- mx-auto py-[6em]">
+    <div className="max-w-5xl bg-gray-900 text-white sm:px-6 lg:px-8 px-4 h- mx-auto py-10">
       <PurpleHeading className="text-4xl mb-4">About skillcert</PurpleHeading>
       <p className="text-lg text-gray-300 mb-12">
         Empowering learners with cutting-edge online courses
@@ -36,7 +36,7 @@ export default function AboutSkillCert() {
         What is skillcert ?
       </PurpleHeading>
 
-      <p className="text-gray-300 leading-relaxed mb-12 text-base">
+      <p className="text-gray-300 sm:text-lg md:text-xl leading-relaxed text-base">
         SkillCert is a revolutionary platform that issues NFT-based digital
         certificates on the Stellar blockchain, ensuring authenticity,
         traceability, and forgery resistance. Designed for universities,
@@ -46,11 +46,11 @@ export default function AboutSkillCert() {
         automated, validated certificate issuance.
       </p>
 
-      <PurpleHeading className="text-3xl mb-6">
+      <PurpleHeading className="text-xl sm:text-2xl md:text-3xl my-6">
         Innovation education
       </PurpleHeading>
 
-      <ul className="space-y-3 text-gray-300">
+      <ul className="space-y-3 mt-4 text-sm sm:text-base text-gray-300">
         {features.map((feature, index) => (
           <ListItem key={index}>{feature}</ListItem>
         ))}
